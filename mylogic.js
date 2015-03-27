9ebf9214b0b5c3905be35bf517e543378af9bd74
Ten Mutunhire <tmutunhire@gmail.com>
2015-03-27 21:57:15 +0000

    try easywebrtc

---
+++ mylogic.js
@@ -0,0 +1,40 @@
+function my_init() {
+         easyrtc.setRoomOccupantListener( loggedInListener);
+         easyrtc.easyApp("Company_Chat_Line", "self", ["caller"],
+             function(myId) {
+                console.log("My easyrtcid is " + myId);
+             }
+         );
+     }
+
+
+     function loggedInListener(roomName, otherPeers) {
+        var otherClientDiv = document.getElementById('otherClients');
+        while (otherClientDiv.hasChildNodes()) {
+            otherClientDiv.removeChild(otherClientDiv.lastChild);
+        }
+        for(var i in otherPeers) {
+            var button = document.createElement('button');
+            button.onclick = function(easyrtcid) {
+                return function() {
+                    performCall(easyrtcid);
+                }
+            }(i);
+
+            label = document.createTextNode(i);
+            button.appendChild(label);
+            otherClientDiv.appendChild(button);
+        }
+    }
+
+
+    function performCall(easyrtcid) {
+        easyrtc.call(
+           easyrtcid,
+           function(easyrtcid) { console.log("completed call to " + easyrtcid);},
+           function(errorMessage) { console.log("err:" + errorMessage);},
+           function(accepted, bywho) {
+              console.log((accepted?"accepted":"rejected")+ " by " + bywho);
+           }
+       );
+    }
