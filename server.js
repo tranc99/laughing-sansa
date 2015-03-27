8b4da421be496fa66685cf764f9591e4f6729d46
Ten Mutunhire <tmutunhire@gmail.com>
2015-03-27 22:17:28 +0000

    changes for Heroku

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var httpApp = express();
 httpApp.use(express.static(__dirname));
 
 // Start Express http server on port 8080
-var webServer = http.createServer(httpApp).listen(8080);
+var webServer = http.createServer(httpApp).listen(5000);
 
 // Start Socket.io so it attaches itself to Express server
 var socketServer = io.listen(webServer, {"log level":1});
